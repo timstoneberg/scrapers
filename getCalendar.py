91ed3a1d540946f43aa501acfdce3e199a7b643b
Tim Stoneberg <tstoneberg@luc.edu>
2019-09-30 14:39:18 -0500

    Modified to allow for individually modified calendar items.

--- getCalendar.py
+++ getCalendar.py
@@ -29,7 +29,8 @@ g = urllib.urlopen(url).read().decode('UTF-8')
 googleCal=Calendar.from_ical(g)
 
 # Setup variables to store the events of various types
-eventIDs = []
+eventUIDs = []
+eventCustomUIDs = []
 eventDates = []
 eventStartTimes = []
 eventStartHours = []
@@ -70,14 +71,29 @@ startDate = (monday.year, monday.month, monday.day)
 endDate = (saturday.year, saturday.month, saturday.day)
 
 events = recurring_ical_events.of(googleCal).between(startDate, endDate)
-eventID = 0
-for event in events:
-    eventID = eventID + 1
-    if type(event['DTSTART'].dt) is datetime.datetime:
 
-        dtStart = event['DTSTART'].dt.astimezone(toZone)
-        dtEnd = event['DTEND'].dt.astimezone(toZone)
-        name = event["SUMMARY"]
+class MyCalEvent:
+    def __init__(self):
+        self.uid = None
+        self.customUID = None
+        self.name = None
+        self.date = None
+        self.start = None
+        self.end = None
+        self.details = None
+        self.isAllDay = None
+        self.dayOfWeek = None
+        self.startMinute = None
+        self.startHour = None
+        self.endMinute = None
+        self.endHour = None
+        self.dw = None
+        self.dayOfWeek = None
+
+    def createTimedEvent(self, calEvent):
+        dtStart = calEvent['DTSTART'].dt.astimezone(toZone)
+        dtEnd = calEvent['DTEND'].dt.astimezone(toZone)
+        name = calEvent["SUMMARY"]
         date = dtStart.date()
 
         minute = str(dtStart.minute)
@@ -85,8 +101,10 @@ for event in events:
             minute = "00"
         elif dtStart.minute < 10:
             minute = "0" + str(dtStart.minute)
+
         startMinute = int(minute)
         startHour = int(dtStart.hour)
+
         if startHour > 12:
             start = str(dtStart.hour - 12) + ":" + minute + " pm"
         else:
@@ -104,7 +122,7 @@ for event in events:
         else:
             end = str(dtEnd.hour) + ":" + minute + " am"
 
-        details = event["DESCRIPTION"]
+        details = calEvent["DESCRIPTION"]
         isAllDay = False
         dw = str(dtStart.weekday())
 
@@ -126,63 +144,120 @@ for event in events:
         else:
             dayOfWeek = None
 
-        eventIDs.append(eventID)
-        eventNames.append(name)
-        eventDates.append(date)
-        eventStartTimes.append(start)
-        eventEndTimes.append(end)
-        eventDetails.append(details)
-        eventAllDays.append(isAllDay)
-        eventDayOfWeeks.append(dayOfWeek)
-        eventStartHours.append(startHour)
-        eventStartMinutes.append(startMinute)
-        eventEndHours.append(endHour)
-        eventEndMinutes.append(endMinute)
+        self.uid = calEvent['UID']
+        self.name = name
+        self.date = date
+        self.customUID = str(self.uid) + str(self.date)
+        self.start = start
+        self.end = end
+        self.details = details
+        self.isAllDay = isAllDay
+        self.dayOfWeek = dayOfWeek
+        self.startMinute = startMinute
+        self.startHour = startHour
+        self.endMinute = endMinute
+        self.endHour = endHour
+        self.dw = dw
+        self.dayOfWeek = dayOfWeek
 
-    else:
-        name = event["SUMMARY"]
-        date = event["DTSTART"].dt
-        start = None
-        end = None
-        startMinute = None
-        startHour = None
-        details = event["DESCRIPTION"]
-        isAllDay = True
-        dw = str(event['DTSTART'].dt.weekday())
 
-        if dw == "0":
-            dayOfWeek = "Monday"
-        elif dw == "1":
-            dayOfWeek = "Tuesday"
-        elif dw == "2":
-            dayOfWeek = "Wednesday"
-        elif dw == "3":
-            dayOfWeek = "Thursday"
-        elif dw == "4":
-            dayOfWeek = "Friday"
-        elif dw == "5":
-            dayOfWeek = "Saturday"
-        elif dw == "6":
-            dayOfWeek = "Sunday"
+    def createAllDayEvent(self, calEvent):
+        self.name = calEvent["SUMMARY"]
+        self.uid = calEvent['UID']
+        self.date = calEvent["DTSTART"].dt
+        self.customUID = str(self.uid) + str(self.date)
+        self.start = None
+        self.end = None
+        self.startMinute = None
+        self.startHour = None
+        self.endMinute = None
+        self.endHour = None
+        self.details = calEvent["DESCRIPTION"]
+        self.isAllDay = True
+        self.dw = str(calEvent['DTSTART'].dt.weekday())
+
+        if self.dw == "0":
+            self.dayOfWeek = "Monday"
+        elif self.dw == "1":
+            self.dayOfWeek = "Tuesday"
+        elif self.dw == "2":
+            self.dayOfWeek = "Wednesday"
+        elif self.dw == "3":
+            self.dayOfWeek = "Thursday"
+        elif self.dw == "4":
+            self.dayOfWeek = "Friday"
+        elif self.dw == "5":
+            self.dayOfWeek = "Saturday"
+        elif self.dw == "6":
+            self.dayOfWeek = "Sunday"
         else:
-            dayOfWeek = None
+            self.dayOfWeek = None
 
-        eventIDs.append(eventID)
-        eventNames.append(name)
-        eventDates.append(date)
-        eventStartTimes.append(start)
-        eventEndTimes.append(end)
-        eventDetails.append(details)
-        eventAllDays.append(isAllDay)
-        eventDayOfWeeks.append(dayOfWeek)
-        eventStartHours.append(startHour)
-        eventStartMinutes.append(startMinute)
-        eventEndHours.append(endHour)
-        eventEndMinutes.append(endMinute)
+
+allEvents = {}
+
+
+for event in events:
+    newEvent = MyCalEvent()
+    if type(event['DTSTART'].dt) is datetime.datetime:
+        newEvent.createTimedEvent(event)
+    else:
+        newEvent.createAllDayEvent(event)
+
+    allEvents[newEvent.customUID] = newEvent
+
+
+# Next we get the non-recurring events for the current day
+for event in googleCal.walk('VEVENT'):
+    newEvent = MyCalEvent()
+
+    if type(event['DTSTART'].dt) is datetime.date:
+        eventDate = event['DTSTART'].dt
+        testUID = str(event['UID']) + str(eventDate)
+
+        if eventDate >= monday.date() and eventDate <= saturday.date():
+            if testUID in allEvents:
+                newEvent.createAllDayEvent(event)
+                allEvents[newEvent.customUID] = newEvent
+                print("Overwriting the existing event with modified item in a recurring event")
+            else:
+                newEvent.createAllDayEvent(event)
+                allEvents[newEvent.customUID] = newEvent
+                print("Adding a new single use event")
+    else:
+        eventDate = event['DTSTART'].dt.date()
+        testUID = str(event['UID']) + str(eventDate)
+
+        if eventDate >= monday.date() and eventDate <= saturday.date():
+            if testUID in allEvents:
+                newEvent.createTimedEvent(event)
+                allEvents[newEvent.customUID] = newEvent
+                print("Overwriting the existing event with modified item in a recurring event")
+            else:
+                newEvent.createTimedEvent(event)
+                allEvents[newEvent.customUID] = newEvent
+                print("Adding a new single use event")
+
+
+# Iterate the dictionary to put values into array, because that's what pandas likes
+for item in allEvents:
+    eventUIDs.append(allEvents[item].uid)
+    eventNames.append(allEvents[item].name)
+    eventDates.append(allEvents[item].date)
+    eventStartTimes.append(allEvents[item].start)
+    eventEndTimes.append(allEvents[item].end)
+    eventDetails.append(allEvents[item].details)
+    eventAllDays.append(allEvents[item].isAllDay)
+    eventDayOfWeeks.append(allEvents[item].dayOfWeek)
+    eventStartHours.append(allEvents[item].startHour)
+    eventStartMinutes.append(allEvents[item].startMinute)
+    eventEndHours.append(allEvents[item].endHour)
+    eventEndMinutes.append(allEvents[item].endMinute)
+    eventCustomUIDs.append(allEvents[item].customUID)
 
 # Now we put the results into a data frame
 # Put results into DataFrame
-df = pd.DataFrame({"id": eventIDs, "eventDate": eventDates, "eventStartTime": eventStartTimes, "eventEndTime": eventEndTimes, "eventName": eventNames, "eventDetails": eventDetails, "eventIsAllDay": eventAllDays, "eventDayOfWeek": eventDayOfWeeks,"eventStartHour": eventStartHours,"eventStartMinute": eventStartMinutes,"eventEndHour": eventEndHours,"eventEndMinute": eventEndMinutes})
+df = pd.DataFrame({"uid": eventUIDs, "cuid": eventCustomUIDs, "eventDate": eventDates, "eventStartTime": eventStartTimes, "eventEndTime": eventEndTimes, "eventName": eventNames, "eventDetails": eventDetails, "eventIsAllDay": eventAllDays, "eventDayOfWeek": eventDayOfWeeks,"eventStartHour": eventStartHours,"eventStartMinute": eventStartMinutes,"eventEndHour": eventEndHours,"eventEndMinute": eventEndMinutes})
 
 # Connect to the MSSQL Server
 engine = create_engine('mssql+pyodbc:///?odbc_connect=%s' % params)
@@ -190,52 +265,3 @@ print("Writing Calendar entries to SQL Server")
 df.to_sql(name='calendarEvents', con=engine, if_exists='replace', index=False)
 
 print("Done")
-
-# Next we get the non-recurring events for the current day
-# if 1 == 1:
-#     for event in googleCal.walk('VEVENT'):
-#         if type(event['DTSTART'].dt) is datetime:
-#             if event['DTSTART'].dt.date() == today.date():
-#                 print(event['SUMMARY'])
-#                 # print(component.get('dtstart'))
-#                 # print(component.get('dtend'))
-#                 # print(component.get('dtstamp'))
-#                 print(event['DTSTART'].dt)
-#         elif event['DTSTART'].dt == today.date():
-#             print("Outlier")
-#             print(event['SUMMARY'])
-#             print(event['DTSTART'].dt)
-
-
-
-
-
-
-
-
-
-
-
-
-    # if component.name == "VEVENT":
-    #     print(component.get('summary'))
-    #     print(component.get('dtstart'))
-    #     print(component.get('dtend'))
-    #     print(component.get('dtstamp'))
-        # print(component.get('dtstart'))
-        # print(component.get('dtend'))
-        # print(component.get('dtstamp'))
-
-#print(g)
-
-
-#c = Calendar(urllib.urlopen(url).read().decode())
-#c = Calendar(urlopen(url).read().decode())
-
-#import re
-
-#text =  c
-#print(re.search("SUMMARY:.*?:", text, re.DOTALL).group())
-#print(re.search("DESCRIPTION:.*?:", text, re.DOTALL).group())
-#print(re.search("DTSTAMP:.*:?", text, re.DOTALL).group())
-#print(re.search("SUMMARY"))
